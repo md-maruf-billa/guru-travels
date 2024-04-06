8ff89721d6dc6b585455592e17b4d14637b5232d
eng-maruf <142441055+eng-maruf@users.noreply.github.com>
2024-04-06 14:26:11 +0600

    Added Home Page with Beautiful Slide

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,99 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { FaArrowLeft, FaArrowRight} from "react-icons/fa";
+import './home.css'
 
 const Home = () => {
+    useEffect(() => {
+        const next = document.querySelector('.next');
+        const prev = document.querySelector('.prev');
+
+        const nextClickHandler = () => {
+            const items = document.querySelectorAll('.item');
+            document.querySelector('.slide').appendChild(items[0]);
+        };
+
+        const prevClickHandler = () => {
+            const items = document.querySelectorAll('.item');
+            const slide = document.querySelector('.slide');
+            slide.prepend(items[items.length - 1]);
+        };
+
+        next.addEventListener('click', nextClickHandler);
+        prev.addEventListener('click', prevClickHandler);
+
+        // Cleanup
+        return () => {
+            next.removeEventListener('click', nextClickHandler);
+            prev.removeEventListener('click', prevClickHandler);
+        };
+    }, []);
     return (
-        <div>
-            This is Home............
+        <div className=''>
+            <div className="slide-container container min-h-screen">
+                <div className="slide">
+                    <div className="item" style={{ backgroundImage: 'url(https://i.ibb.co/qCkd9jS/img1.jpg)' }}>
+                        <div className="content">
+                            <div className="name">Switzerland</div>
+                            <div className="des">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab, eum!</div>
+                            <button>See More</button>
+                        </div>
+                    </div>
+                    <div className="item" style={{ backgroundImage: 'url(https://i.ibb.co/jrRb11q/img2.jpg)' }}>
+                        <div className="content">
+                            <div className="name">Finland</div>
+                            <div className="des">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab, eum!</div>
+                            <button>See More</button>
+                        </div>
+                    </div>
+                    <div className="item" style={{ backgroundImage: 'url(https://i.ibb.co/NSwVv8D/img3.jpg)' }}>
+                        <div className="content">
+                            <div className="name">Iceland</div>
+                            <div className="des">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab, eum!</div>
+                            <button>See More</button>
+                        </div>
+                    </div>
+                    <div className="item" style={{ backgroundImage: 'url(https://i.ibb.co/Bq4Q0M8/img4.jpg)' }}>
+                        <div className="content">
+                            <div className="name">Australia</div>
+                            <div className="des">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab, eum!</div>
+                            <button>See More</button>
+                        </div>
+                    </div>
+                    <div className="item" style={{ backgroundImage: 'url(https://i.ibb.co/jTQfmTq/img5.jpg)' }}>
+                        <div className="content">
+                            <div className="name">Netherland</div>
+                            <div className="des">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab, eum!</div>
+                            <button>See More</button>
+                        </div>
+                    </div>
+                    <div className="item" style={{ backgroundImage: 'url(https://i.ibb.co/RNkk6L0/img6.jpg)'}}>
+                        <div className="content">
+                            <div className="name">Ireland</div>
+                            <div className="des">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab, eum!</div>
+                            <button>See More</button>
+                        </div>
+                    </div>
+                    <div className="item" style={{ backgroundImage: 'url(https://i.postimg.cc/RFfBhxPZ/Rectangle-1.png)' }}>
+                        <div className="content">
+                            <div className="name">Cox's bazar</div>
+                            <div className="des">Cox's Bazar is a city, fishing port, tourism centre and district headquarters in southeastern Bangladesh. It is famous mostly for its long natural sandy beach, and it ...</div>
+                            <button>See More</button>
+                        </div>
+                    </div>
+                    <div className="item" style={{ backgroundImage: 'url(https://i.postimg.cc/fR6qLt1Y/Sreemongol.png)' }}>
+                        <div className="content">
+                            <div className="name">Ireland</div>
+                            <div className="des">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab, eum!</div>
+                            <button>See More</button>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="button">
+                    <button className="prev flex justify-center items-center"><FaArrowLeft /></button>
+                    <button className="next flex justify-center items-center"><FaArrowRight /></button>
+                </div>
+            </div>
         </div>
     );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ content: [
 './src/**/*.{js,ts,jsx,tsx}',
 ],
 theme: {
-extend: {},
+extend: {
+    fontFamily:{
+        title:"'Bebas Neue', sans-serif",
+        pfont:"'Montserrat', sans-serif"
+    }
+},
 },
 plugins: [require('daisyui')],
 }
